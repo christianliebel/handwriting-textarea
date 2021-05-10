e9cb6752cfa94db3a9f905686195709953e6fd4d
Christian Liebel <christian.liebel@thinktecture.com>
2021-05-10 11:50:12 +0200

    feat(element): add canvas and register for pointer events

--- src/HandwritingTextarea.ts
+++ src/HandwritingTextarea.ts
@@ -1,14 +1,22 @@
-import { html, LitElement } from 'lit';
+import { css, html, LitElement } from 'lit';
 import { property, state } from 'lit/decorators.js';
 
 /* global HandwritingModelConstraint, HandwritingRecognizer */
 
+// TODO: Forms participation? https://web.dev/more-capable-form-controls/#defining-a-form-associated-custom-element
 export class HandwritingTextarea extends LitElement {
+  static readonly styles = css`
+    canvas {
+      border: 1px solid black;
+      touch-action: none;
+    }
+  `;
+
   @property({ type: String }) languages: string = 'en';
 
-  #recognizer?: HandwritingRecognizer;
-
   @state() supported = false;
+
+  #recognizer?: HandwritingRecognizer;
 
   async connectedCallback() {
     super.connectedCallback();
@@ -21,6 +29,8 @@ export class HandwritingTextarea extends LitElement {
       languages: this.languages.split(','),
     } as HandwritingModelConstraint;
     try {
+      // TODO: Re-initialize recognizer on property changes?
+      // TODO: Add API to query support? (queryHandwritingRecognizerSupport)
       this.#recognizer = await navigator.createHandwritingRecognizer(
         constraint
       );
@@ -36,11 +46,30 @@ export class HandwritingTextarea extends LitElement {
     this.supported = true;
   }
 
+  __onPointerDown(evt: PointerEvent) {
+    console.log(evt, this.#recognizer);
+  }
+
+  __onPointerMove(evt: PointerEvent) {
+    console.log(evt, this.#recognizer);
+  }
+
+  __onPointerUp(evt: PointerEvent) {
+    console.log(evt, this.#recognizer);
+  }
+
   render() {
+    const canvas = html` <canvas
+      @pointerdown="${(evt: PointerEvent) => this.__onPointerDown(evt)}"
+      @pointermove="${(evt: PointerEvent) => this.__onPointerMove(evt)}"
+      @pointerup="${(evt: PointerEvent) => this.__onPointerUp(evt)}"
+    ></canvas>`;
+
     // TODO: Pass-through textarea attributes?
     return html`
       <textarea cols="60" rows="5"></textarea>
       <h2>Handwriting supported? ${this.supported}</h2>
+      ${this.supported ? canvas : ''}
     `;
   }
 
