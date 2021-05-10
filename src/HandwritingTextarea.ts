a140c557dcc7039c789bf9df4fc124ce75cdf037
Christian Liebel <christian.liebel@thinktecture.com>
2021-05-10 11:18:59 +0200

    feat(element): add feature detection

--- src/HandwritingTextarea.ts
+++ src/HandwritingTextarea.ts
@@ -1,27 +1,15 @@
-import { css, html, LitElement } from 'lit';
-import { property } from 'lit/decorators.js';
+import { html, LitElement } from 'lit';
 
 export class HandwritingTextarea extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-      padding: 25px;
-      color: var(--handwriting-textarea-text-color, #000);
-    }
-  `;
-
-  @property({ type: String }) title = 'Hey there';
-
-  @property({ type: Number }) counter = 5;
-
-  __increment() {
-    this.counter += 1;
+  static get handwritingSupported() {
+    return 'createHandwritingRecognizer' in navigator;
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <h2>
+        Handwriting supported? ${HandwritingTextarea.handwritingSupported}
+      </h2>
     `;
   }
 }
